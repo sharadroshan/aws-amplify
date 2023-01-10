1b7015cc7e1daa75e7afdd0672a11923e4cb113a
sharadroshan <sharadroshan96.user@gmail.com>
2023-01-10 12:43:12 +0530

    update Design

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import AuthenticatorWithEmail from './EmployeeDetail/AuthenticatorWithEmail'
 import MenuButton from './EmployeeDetail/MenuButton';
+import Design from './EmployeeDetail/Design';
 
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div>
+      <Design />
       <MenuButton />
       <AuthenticatorWithEmail />
     </div>
