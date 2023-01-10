4b300ba9828253fdc9547549c29eaf36093c15ba
sharadroshan <sharadroshan96.user@gmail.com>
2023-01-10 13:08:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import AuthenticatorWithEmail from './EmployeeDetail/AuthenticatorWithEmail'
-import MenuButton from './EmployeeDetail/MenuButton';
-import Design from './EmployeeDetail/Design';
 
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div>
-      <Design />
-      <MenuButton />
       <AuthenticatorWithEmail />
     </div>
   );
--- src/EmployeeDetail/AuthenticatorWithEmail.js
+++ src/EmployeeDetail/AuthenticatorWithEmail.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Amplify, Auth } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, SwitchField } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { Button } from '@mui/material';
 
 export default function AuthenticatorWithEmail() {
     const services = {
@@ -25,7 +24,7 @@ export default function AuthenticatorWithEmail() {
         <div>
             <h1>Hello</h1>
             <Authenticator services={services} initialState="signUp">
-                {({ signOut }) => <Button variant='contained' onClick={signOut}>Sign out</Button>}
+                {({ signOut }) => <SwitchField variant='contained' onClick={signOut}></SwitchField>}
             </Authenticator>
         </div>
     );
